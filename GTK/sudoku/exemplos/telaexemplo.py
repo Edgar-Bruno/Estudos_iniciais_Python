38bb5ec8df328d0acbdb31423b3b5e1c3640af09
Edgar Bruno <edgar.brunos@gmail.com>
2016-11-27 16:55:43 +0000

    Implementação da lógica para montrar a matriz do jogo.

--- GTK/sudoku/exemplos/telaexemplo.py
+++ GTK/sudoku/exemplos/telaexemplo.py
@@ -14,7 +14,7 @@ class Aplicacao:
 		self.window.set_title("Sudoku")
 		self.window.connect('destroy', lambda w: gtk.main_quit())
 		self.window.set_position(gtk.WIN_POS_CENTER_ALWAYS)
-		self.window.set_size_request(500, 500)
+		#self.window.set_size_request(550, 550)
 
 		self.create_widgets()
 
@@ -23,31 +23,61 @@ class Aplicacao:
 
 	def create_widgets(self):
 
-		self.hbox = gtk.HBox(False, 50)
+		self.hbox = gtk.HBox(True, 50)
 		self.table = gtk.Table(3,3, gtk.TRUE)
 
+
+		listaMatriz =[6, 2, 4, 0, 1, 5, 9, 0, 7, 3, 8,
+					 9, 7, 8, 0, 2, 6, 3, 0, 1, 5, 4,
+					 5, 6, 2, 0, 3, 8, 7, 0, 4, 9, 1,
+					 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+					 1, 5, 7, 0, 8, 3, 4, 0, 2, 6, 9,
+					 4, 9, 1, 0, 6, 7, 8, 0, 5, 2, 3,
+					 7, 8, 5, 0, 9, 1, 2, 0, 3, 4, 6,
+					 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+					 3, 4, 9, 0, 7, 2, 6, 0, 8, 1, 5,
+					 8, 3, 6, 0, 5, 4, 1, 0, 9, 7, 2,
+					 2, 1, 3, 0, 4, 9, 5, 0, 6, 8, 7]
+
+		#print listaMatriz
 		x = 0
 		y = 0
 
-		for i in range(9):
-			self.table_b = gtk.Table(2,2, gtk.TRUE)
+		posFix = [0, 4 , 8 , 44, 48, 52, 88, 92, 96]
+
+
+
+		for i in posFix:
+			self.table_b = gtk.Table(3,3, gtk.TRUE)
 			self.frame = gtk.Frame()
 	
 			xx = 0
 			yy = 0
 
-			for ii in range(9):
-				var = "[%d, %d]" % (i, ii)
-				self.button = gtk.Button(str(var))
+			#monta o quadrante
+			quadMontado = []
 
+			z = 0
+			for iq in range(3):
+				quadMontado.extend(listaMatriz[i+z:i+3+z])
+				
+				z += 11
+
+			print quadMontado
+
+			for ii in quadMontado:
+				#var = "[%d, %d]" % (i, ii)
+				
+				self.button = gtk.Button(str(ii))
 				self.table_b.attach(self.button, xx, xx+1, yy, yy+1)
+
 				xx += 1
 				if xx > 2:
 					xx = 0
 					yy += 1
 
 			self.frame.add(self.table_b)
-			self.table.attach(self.frame, x, x+1, y, y+1,xpadding=1, ypadding=1)
+			self.table.attach(self.frame, x, x+1, y, y+1,xpadding=3, ypadding=3)
 			x += 1
 			if x > 2:
 				x = 0
